815128db7dc994aa2a83af6256032d86147d9c2c
Cinthia Kazue Arizono <carizono@gmail.com>
2023-03-10 22:18:00 -0300

    Criando tabelas de paciente, raca e cliente

--- manipulacao_dados.sql
+++ manipulacao_dados.sql
@@ -1,8 +1,18 @@
-INSERT INTO cliente (nome) VALUES ('Cinthia');
-INSERT INTO cliente (nome) VALUES ('Fabio');
-INSERT INTO cliente (nome) VALUES ('Lucas');
-SELECT* FROM cliente;
-DROP TABLE registro
+CREATE TABLE paciente(
+		id SERIAL PRIMARY KEY,
+		nome VARCHAR(255) NOT NULL,
+		raca_id INTEGER, 
+		FOREIGN KEY (raca_id)
+		REFERENCES raca (id)
+);
+CREATE TABLE raca(
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(255)NOT NULL
+);
+CREATE TABLE cliente(
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(255)NOT NULL
+);
 CREATE TABLE registro (
 	paciente_id INTEGER,
 	cliente_id INTEGER,
@@ -12,8 +22,24 @@ CREATE TABLE registro (
 	PRIMARY KEY (paciente_id, cliente_id)
 	
 );
+
+INSERT INTO raca (nome) VALUES ('SRD');
+INSERT INTO raca (nome) VALUES ('Pisncher');
+INSERT INTO raca (nome) VALUES ('Labrador');
+
+INSERT INTO paciente (nome, raca_id) VALUES ('Toby', 1);
+INSERT INTO paciente (nome, raca_id) VALUES ('Thor', 3);
+INSERT INTO paciente (nome, raca_id) VALUES ('Ciroc', 2);
+
+INSERT INTO cliente (nome) VALUES ('Cinthia');
+INSERT INTO cliente (nome) VALUES ('Fabio');
+INSERT INTO cliente (nome) VALUES ('Lucas');
+
+SELECT* FROM cliente;
+
 INSERT INTO registro(paciente_id, cliente_id)VALUES(1,1);
 INSERT INTO registro(paciente_id, cliente_id)VALUES(2,3);
+
 
 SELECT paciente.nome AS "Nome do paciente",
 	   cliente.nome AS "Nome do cliente"
